8ed6feb3d49521622aa173035e9e9f379be711d7
Inès C <ines.parisdescartes@gmail.com>
2022-10-16 18:35:11 +0200

    flag project

---
+++ projet-1/src/pages/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation/>
+            <h1>À propos</h1>
+            <br />
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Non corrupti sed tempora minima repellendus eveniet in quibusdam ut 
+                quae nam commodi ratione repudiandae quod aliquid dolore ab, 
+                voluptatibus totam pariatur culpa. Nam incidunt, qui vitae quo 
+                laudantium voluptatibus consequatur, quam exercitationem commodi 
+                aliquam porro unde earum nihil aut esse ratione deserunt ducimus 
+                minima pariatur quod nesciunt. Beatae quisquam vel ut earum inventore 
+                quasi praesentium unde ipsum fugit maxime sapiente, reprehenderit qui ex. 
+                Consectetur dolorum, deleniti, veniam doloribus mollitia omnis quam 
+                dolorem corporis tenetur distinctio quasi, voluptas nemo expedita 
+                placeat sunt earum error ducimus excepturi iste magni beatae? 
+                Perspiciatis, maiores error.</p>
+        </div>
+    );
+};
+
+export default About;
